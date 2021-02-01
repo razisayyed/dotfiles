64aeee544638634fd015bef60ba0c3b770c315b1
Razi Alsayyed <razi@najah.edu>
2021-02-01 11:58:32 +0200

    Initial commit

---
+++ .config/fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,6 @@
+set -g VIRTUALFISH_VERSION 2.5.1
+set -g VIRTUALFISH_PYTHON_EXEC /usr/bin/python
+source /usr/lib/python3.9/site-packages/virtualfish/virtual.fish
+source /usr/lib/python3.9/site-packages/virtualfish/compat_aliases.fish
+source /usr/lib/python3.9/site-packages/virtualfish/auto_activation.fish
+emit virtualfish_did_setup_plugins
---
+++ .config/fish/functions/br.fish
@@ -0,0 +1,1 @@
+/home/razi/.local/share/broot/launcher/fish/br.fish
